85016f16c24cd56d8edb0c714cad98321811d229
Prit5656 <55257845+Prit5656@users.noreply.github.com>
2025-01-15 18:05:12 -0500

    second chanebe

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
 })
